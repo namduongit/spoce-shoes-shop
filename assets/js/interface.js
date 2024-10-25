1c46d317434486b21e92add1b2e5ffc6fb1949c2
Nguyễn Nam Dương <3123560012@sv.sgu.edu.vn>
2024-10-25 14:06:58 +0700

    assets folder in front-end branch

---
+++ assets/js/interface.js
@@ -0,0 +1,225 @@
+function writeLogoAndUserTools() {
+    document.write(`
+    <header>
+        <div class="inner-wrap">
+            <div class="inner-content">
+                <div class="inner-logo">
+                    <a href="">
+                        <img src="assets/image/logo/logo-header-2.png" alt="">
+                    </a>
+                </div>
+
+                <div class="inner-search">
+                    <select name="" id="">
+                        <option value="" id="all-products" selected>Tất cả</option>
+                        <option value="" id="nike-products">NIKE</option>
+                        <option value="" id="adidas-products">ADIDAS</option>
+                        <option value="" id="vans-products">VANS</option>
+                        <option value="" id="converse-products">CONVERSE</option>
+                        <option value="" id="clothes-products">QUẦN ÁO</option>
+                        <option value="" id="other-products">PHỤ KIỆN</option>
+                    </select>
+                    <div class="search">
+                        <input type="search" placeholder="Tìm kiếm sản phẩm">
+                        <a href="">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="inner-user">
+                    <div class="user">
+                        <div class="parent">
+
+                            <i class="fa-solid fa-user"></i>
+                            <span>Tài khoản</span>
+
+                            <div class="child">
+                                <div class="child-login" onclick="showTools(this)" id="login">
+                                    <i class="fa-solid fa-right-to-bracket"></i>
+                                    <span>Đăng nhập</span>
+                                </div>
+                                <div class="child-register" onclick="showTools(this)" id="register">
+                                    <i class="fa-solid fa-user-plus"></i>
+                                    <span>Đăng ký</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="cart">
+                        <div>
+                            <i class="fa-solid fa-cart-shopping"></i>
+                            <span>Giỏ hàng</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    `);
+}
+
+function writeMenuSelection() {
+    document.write(`
+    <div class="section-one">
+        <div class="inner-wrap">
+            <div class="inner-content">
+                <ul class="menu-1">
+                    <li>
+                        <a href="">All</a>
+                    </li>
+                    <li>
+                        <a href="">Sale 40% - 80%</a>
+                    </li>
+                    <li>
+                        <a href="">NIKE</a>
+                    </li>
+                    <li>
+                        <a href="">ADIDAS</a>
+                    </li>
+                    <li>
+                        <a href="">VANS</a>
+                    </li>
+                    <li>
+                        <a href="">CONVERSE</a>
+                    </li>
+                    <li>
+                        <a href="">HÃNG KHÁC</a>
+                    </li>
+                    <li>
+                        <a href="">QUẦN ÁO</a>
+                    </li>
+                    <li>
+                        <a href="">PHỤ KIỆN</a>
+                    </li>
+                    <li>
+                        <a href="">NHẬN THÔNG BÁO SALE</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `);
+}
+
+function writeSlideShow() {
+    document.write(`
+    <div class="section-two">
+        <div class="inner-slide-show">
+            <img src="assets/image/slide-show/slider_1.png" alt="" id="slide-show">
+        </div>
+    </div>
+    `);
+}
+
+function writeStorePolicies() {
+    document.write(`
+    <footer class="footer">
+        <div class="inner-wrap">
+            <div class="inner-content">
+                <div class="inner-left">
+                    <div class="title">ĐỒ ÁN MÔN WEB 1</div>
+                    <div class="address">Địa chỉ: 273 An Dương Vương Phường 3, Quận 5</div>
+                    <br>
+                    <div class="number-phone">Số điện thoại: 0388.853.835</div>
+                    <br>
+                    <img src="assets/image/logo/logo-shop-1.png" alt="">
+                </div>
+                <div class="inner-mid-1">
+                    <div class="title">Thông tin</div>
+                    <ul>
+                        <li>
+                            <a href="">Trang chủ</a>
+                        </li>
+                        <li>
+                            <a href="">Giới thiệu</a>
+                        </li>
+                        <li>
+                            <a href="">Sản phẩm</a>
+                        </li>
+                        <li>
+                            <a href="">Khuyến mãi</a>
+                        </li>
+                        <li>
+                            <a href="">Tin tức</a>
+                        </li>
+                        <li>
+                            <a href="">Liên hệ</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="inner-mid-2">
+                    <div class="title">Hướng dẫn & Chính sách</div>
+                    <ul>
+                        <li>
+                            <a href="">Hướng dẫn mua hàng</a>
+                        </li>
+                        <li>
+                            <a href="">Thẻ Thành Viên</a>
+                        </li>
+                        <li>
+                            <a href="">Chính sách đổi trả & hoàn tiền</a>
+                        </li>
+                        <li>
+                            <a href="">Chính sách thanh toán</a>
+                        </li>
+                        <li>
+                            <a href="">Chính sách vận chuyển</a>
+                        </li>
+                        <li>
+                            <a href="">Chính sách bảo mật</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="inner-right">
+                    <h3>Đồ án môn "Lập trình web và ứng dụng </h2>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.556685064342!2d106.67967527485665!3d10.759992589387783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1c81c64183%3A0xd3109d7a7a8f753c!2zMjczIMSQLiBBbiBExrDGoW5nIFbGsMahbmcsIFBoxrDhu51uZyAzLCBRdeG6rW4gNSwgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e1!3m2!1svi!2s!4v1729153291461!5m2!1svi!2s"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </footer>
+    `);
+}
+
+function writeContactUs() {
+    document.write(`
+    <div class="inner-footer">
+        <div class="inner-wrap">
+            <div class="inner-content">
+                <div class="inner-left">
+                    <div>© Đồ án của nhóm: <span>Tên nhóm</span></div>
+
+                    <div>Thiết kế website bán hàng bởi namduongit</div>
+                </div>
+
+                <div class="inner-right">
+                    <span>Liên hệ với chúng tôi tại:</span>
+                    <ul>
+                        <li>
+                            <a href="">
+                                <i class="fa-brands fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fa-brands fa-github"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
+}
---
+++ assets/js/show.js
@@ -0,0 +1,147 @@
+function writeForm(funcForm1, funcForm2, funcForm3) {
+    document.getElementById("form-container").innerHTML = `
+    <div class="user-content-selection" id="form">
+        <div class="inner-content">
+            <div class="left-card">
+
+                <div class="inner-title">SPOCE SHOES</div>
+
+                    <div class="inner-desc">Spoce shop là hệ thống chuyên bán các sản phẩm như giày của các hãng nổi tiếng
+                        <strong>ADIDAS</strong>
+                        <strong>NIKE</strong>
+                        <strong>VANS</strong>
+                        <strong>CONVERSE ,...</strong>
+                        Ngoài ra
+                        <strong>SPOCE SHOP</strong>
+                        còn bán các sản phẩm như quần, áo, phụ kiện,... chính hãng uy tín chất lượng, đển mỗi khi bạn đi
+                        ra ngoài nó sẽ giúp bạn trở nên phong độ, tự tin hơn với phong cách trang phục của bạn.
+                        Hệ thống <strong>SPOCE SHOP</strong> còn tuyển các đại lý mua bán sỉ lẻ toàn quốc.
+                    </div>
+
+                    <div class="inner-end-content">
+                        <strong>Nếu bạn dành 1% uy tín cho chúng tôi, chúng tôi sẽ chứng mình 99% còn lại</strong>
+                        <strong>FROM SPOCE SHOP WITH LOVE ❤️</strong>
+                    </div>
+            </div>
+
+            <div class="right-card">
+                <div class="card-content">
+                    <div class="inner-title large-margin-bottom" id="title-form">Đăng nhập hệ thống</div>
+                    ${funcForm2()}
+
+                    ${funcForm1()}
+
+                    ${funcForm3()}
+                </div>
+                <i class="fa-solid fa-minus exit-form" id="exit-form" onclick="hideTools()"></i>
+            </div>
+        </div>
+    </div>
+    `;
+    document.getElementById("form").style.display = "block";
+}
+
+function writeLoginForm() {
+    return `
+    <div class="login-form">
+        <div class="input-username small-margin-bottom">
+            <label for="username">Tài khoản của bạn:</label>
+            <br>
+            <input type="text" id="username" placeholder="Nhập tài khoản">
+        </div>
+        <div class="input-password small-margin-bottom">
+            <label for="password">Nhập mật khẩu:</label>
+            <br>
+            <input type="password" id="password" placeholder="*********">
+        </div>
+    </div>`;
+}
+
+function writeRegisterForm() {
+    return `
+    <div class="register-form">
+        <div class="left-form">
+            <div class="input-username">
+                <label for="username">Tài khoản của bạn:</label>
+                <br>
+                <input type="text" id="username" placeholder="Nhập tài khoản">
+            </div>
+            <div class="innput-password">
+                <label for="password">Nhập mật khẩu:</label>
+                <br>
+                <input type="password" id="password" placeholder="*********">
+            </div>
+            <div class="confirm-password">
+                <label for="password">Nhập lại mật khẩu:</label>
+                <br>
+                <input type="password" id="password" placeholder="*********">
+            </div>
+        </div>
+        <div class="right-form">
+            <div class="innput-email">
+                <label for="email">Nhập email:</label>
+                <br>
+                <input type="email" id="email" placeholder="Nhập email của bạn:">
+            </div>
+            <div class="innput-number-phone">
+                <label for="numberphone">Nhập số điện thoại:</label>
+                <br>
+                <input type="tel" id="numberphone" placeholder="Nhập số điện thoại:">
+            </div>
+        </div>
+    </div>`;
+}
+
+function writeButtonLogin() {
+    return `
+    <div class="inner-notice small-margin-bottom">
+        <div class="desc">Nếu bạn chưa có tài khoản</div>
+        <button id="register-now" class="register-btn" onclick="actionChangeForm(this)">Đăng ký ngay!</button>
+    </div>
+    `;
+}
+function writeButtonRegister() {
+    return `
+    <div class="inner-notice small-margin-bottom">
+        <div class="desc">Nếu bạn đã có tài khoản</div>
+        <button id="login-now" class="login-btn" onclick="actionChangeForm(this)">Đăng nhập ngay!</button>
+    </div>
+    `;
+}
+function submitLogin() {
+    return `
+    <div class="submit-action">
+        <button class="login-btn" id="action-login">Đăng nhập</button>
+    </div>
+    `;
+}
+function submitRegister() {
+    return `
+    <div class="submit-action">
+        <button class="register-btn" id="action-register">Đăng ký</button>
+    </div>
+    `;
+}
+function actionChangeForm(obj) {
+    if (obj.id === "register-now") {
+        writeForm(writeRegisterForm, writeButtonRegister, submitRegister);
+        document.getElementById("title-form").innerHTML = "Đăng nhập hệ thống";
+    }
+    else if (obj.id === "login-now") {
+        writeForm(writeLoginForm, writeButtonLogin, submitLogin);
+        document.getElementById("title-form").innerHTML = "Đăng ký tài khoản";
+    }
+}
+function showTools(obj) {
+    if (obj.id === "login") {
+        writeForm(writeLoginForm, writeButtonLogin, submitLogin);
+        document.getElementById("title-form").innerHTML = "Đăng ký tài khoản";
+    } else if (obj.id === "register") {
+        writeForm(writeRegisterForm, writeButtonRegister, submitRegister);
+        document.getElementById("title-form").innerHTML = "Đăng nhập hệ thống";
+    }
+}
+
+function hideTools() {
+    document.getElementById("form").style.display = "none";
+}
