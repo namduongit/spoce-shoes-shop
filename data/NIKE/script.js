6fbf6f4066ac6537e3d1e468e21896d2234a5cc6
Thuat3Tuoi <itdontsleep@gmail.com>
2024-11-07 22:43:16 +0700

    add data NIKE

---
+++ data/NIKE/script.js
@@ -0,0 +1,302 @@
+var products_list = [
+	{
+		"id": "1",
+		"name_product": "WMNS AIR JORDAN 1 LOW SE 'REVERSE ICE BLUE'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/1.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "2",
+		"name_product": "Ambush x Air Force 1 Low 'Phantom'",
+		"brand": "nike",
+		"price": "3.300.000₫",
+		"sell": "2.640.000₫",
+		"discount": "30",
+		"image": "image/2.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "3",
+		"name_product": "Air Force 1 'Blue Paisley'",
+		"brand": "nike",
+		"price": "1.700.000₫",
+		"sell": "1.360.000₫",
+		"discount": "30",
+		"image": "image/3.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "4",
+		"name_product": "Air Force 1 Essential Low 'White Gym Red'",
+		"brand": "nike",
+		"price": "1.700.000₫",
+		"sell": "1.360.000₫",
+		"discount": "30",
+		"image": "image/4.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "5",
+		"name_product": "JORDAN LEGACY 312 LOW '25TH ANNIVERSARY'",
+		"brand": "nike",
+		"price": "2.200.000đ",
+		"sell": "1.550.000đ",
+		"discount": "30",
+		"image": "image/5.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "6",
+		"name_product": "Air Force 1 07 Low Light Grey Rice",
+		"brand": "nike",
+		"price": "1.800.000đ",
+		"sell": "1.440.000đ",
+		"discount": "30",
+		"image": "image/6.png",
+		"promo_image": {
+			"image_1": "image/6.1.png",
+			"image_2": "image/6.2.png",
+			"image_3": "image/6.3.png",
+			"image_4": "image/6.4.png"
+		}
+	},
+	{
+		"id": "7",
+		"name_product": "AIR JORDAN 1 LOW 'WHITE UNIVERISTY RED'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/7.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "8",
+		"name_product": "Air Jordan 1 Low ‘Pink Foam’",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/8.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "9",
+		"name_product": "Air Jordan 1 Low 'Light Grey Sail'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/9.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "10",
+		"name_product": "WMNS AIR JORDAN 1 LOW 'JADE SMOKE'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/10.png",
+		"promo_image": {
+			"image_1": "image/10.1.png",
+			"image_2": "image/10.2.png",
+			"image_3": "image/10.3.png",
+			"image_4": "image/10.4.png"
+		}
+	},
+	{
+		"id": "11",
+		"name_product": "TROPHY ROOM X JORDAN 1 RETRO LOW OG SP 'ROOKIE CARD - AWAY'",
+		"brand": "nike",
+		"price": "2.800.000đ",
+		"sell": "1.960.000₫",
+		"discount": "30",
+		"image": "image/11.png",
+		"promo_image": {
+			"image_1": "image/11.1.png",
+			"image_2": "image/11.2.png",
+			"image_3": "image/11.3.png",
+			"image_4": "image/11.4.png"
+		}
+	},
+	{
+		"id": "12",
+		"name_product": "AIR JORDAN 1 MID SE 'WHITE ICE BLUE'",
+		"brand": "nike",
+		"price": "2.400.000đ",
+		"sell": "1.920.000₫",
+		"discount": "30",
+		"image": "image/12.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "13",
+		"name_product": "AIR JORDAN 1 LOW SE 'INDUSTRIAL BLUE SASHIKO'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/13.png",
+		"promo_image": {
+			"image_1": "image/13.1.png",
+			"image_2": "image/13.2.png",
+			"image_3": "image/13.3.png",
+			"image_4": "image/13.4.png"
+		}
+	},
+	{
+		"id": "14",
+		"name_product": "AIR JORDAN 1 LOW SE 'TOKYO 96'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/14.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "15",
+		"name_product": "AIR JORDAN 1 LOW GS 'DESERT BERRY'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/15.png",
+		"promo_image": {
+			"image_1": "image/15.1.png",
+			"image_2": "image/15.2.png",
+			"image_3": "image/15.3.png",
+			"image_4": "image/15.4.png"
+		}
+	},
+	{
+		"id": "16",
+		"name_product": "AIR JORDAN 1 LOW 'TRUE BLUE CEMENT'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/16.png",
+		"promo_image": {
+			"image_1": "image/16.1.png",
+			"image_2": "image/16.2.png",
+			"image_3": "image/16.3.png",
+			"image_4": "image/16.4.png"
+		}
+	},
+	{
+		"id": "17",
+		"name_product": "WMNS AIR JORDAN 1 LOW 'PASTEL PLUM'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/17.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "18",
+		"name_product": "Court Borough Low 2 GS 'Triple White'",
+		"brand": "nike",
+		"price": "1.400.000đ",
+		"sell": "980.000đ",
+		"discount": "30",
+		"image": "image/18.png",
+		"promo_image": {
+			"image_1": "",
+			"image_2": "",
+			"image_3": "",
+			"image_4": ""
+		}
+	},
+	{
+		"id": "19",
+		"name_product": "AIR JORDAN 1 LOW 'AQUATONE'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/19.png",
+		"promo_image": {
+			"image_1": "image/19.1.png",
+			"image_2": "image/19.2.png",
+			"image_3": "image/19.3.png",
+			"image_4": "image/19.4.png"
+		}
+	},
+	{
+		"id": "20",
+		"name_product": "Air Jordan 1 Low 'Gold Swoosh'",
+		"brand": "nike",
+		"price": "2.100.000đ",
+		"sell": "1.470.000đ",
+		"discount": "30",
+		"image": "image/20.png",
+		"promo_image": {
+			"image_1": "image/20.1.png",
+			"image_2": "image/20.2.png",
+			"image_3": "image/20.3.png",
+			"image_4": "image/20.4.png"
+		}
+	}
+];
