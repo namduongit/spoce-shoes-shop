a83d141b850582da267c0104a325e5d43ce70078
Nguyễn Nam Dương <3123560012@sv.sgu.edu.vn>
2024-11-11 06:55:39 +0700

    Add

--- data/products/CLOTHES/script.js
+++ data/products/CLOTHES/script.js
@@ -134,5 +134,77 @@ var products_list = [
 			"image_4": "image/89.4.png"
 		}
 	},
+	{
+		"id": "90",
+		"name_product": "HTAG JACKET VARSITY - ROBOT BLACK WHITE",
+		"brand": "clothes",
+		"price": "1300000",
+		"sell": "780000",
+		"discount": "30",
+		"image": "image/90.png",
+		"promo_image": {
+			"image_1": "image/90.1.png",
+			"image_2": "image/90.2.png",
+			"image_3": "image/90.3.png",
+			"image_4": "image/90.4.png"
+		}
+	},
+	{
+		"id": "91",
+		"name_product": "HTAG BOMBER - BRING BLACK",
+		"brand": "clothes",
+		"price": "990000",
+		"sell": "594000",
+		"discount": "30",
+		"image": "image/91.png",
+		"promo_image": {
+			"image_1": "image/91.1.png",
+			"image_2": "image/91.2.png",
+			"image_3": "image/91.3.png",
+			"image_4": "image/91.4.png"
+		}
+	},
+	{
+		"id": "92",
+		"name_product": "HTAG BOMBER - BRING ORANGE",
+		"brand": "clothes",
+		"price": "990000",
+		"sell": "594000",
+		"discount": "30",
+		"image": "image/92.png",
+		"promo_image": {
+			"image_1": "image/92.1.png",
+			"image_2": "image/92.2.png",
+			"image_3": "image/92.3.png",
+			"image_4": "image/92.4.png"
+		}
+	},
+	{
+		"id": "93",
+		"name_product": "Sweater Kenzo Blue",
+		"brand": "clothes",
+		"price": "800000",
+		"sell": "640000",
+		"discount": "30",
+		"image": "image/93.png",
+		"promo_image": {
+			"image_1": "image/93.1.png",
+			"image_2": "image/93.2.png"
+		}
+	},
+	{
+		"id": "94",
+		"name_product": "Sweater Kenzo Black Blue",
+		"brand": "clothes",
+		"price": "800000",
+		"sell": "640000",
+		"discount": "30",
+		"image": "image/94.png",
+		"promo_image": {
+			"image_1": "image/94.1.png",
+			"image_2": "image/94.2.png"
+		}
+	},
+
 
 ]
