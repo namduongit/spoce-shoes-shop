cd12574d93225c8c7bace287955eb9aadff5a50d
cuong75201 <cuong75201@gmail.com>
2024-11-15 18:45:24 +0700

    add onclick details

--- data/js/showProductByCategory.js
+++ data/js/showProductByCategory.js
@@ -43,7 +43,7 @@ function showCategory(value) {
 
     let s = "";
     currentProducts.forEach((product) => {
-      s += `<div class="grid_col-4 product__item">
+      s += `<div class="grid_col-4 product__item" onclick="DetailProducts('${product.id}')">
                         <a href="" class="product__link">
                             <img src="${product.image}" alt="" class="product__link-img">
                             <span class="product__link-name">${product.name_product}</span>
